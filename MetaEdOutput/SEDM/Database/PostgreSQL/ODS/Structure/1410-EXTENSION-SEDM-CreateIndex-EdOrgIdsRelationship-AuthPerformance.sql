c995eb33d907be47bbfb201887d58aedf1076193
Rosh Dhanawade - EA <rdhanawade@edanalytics.org>
2025-10-28 16:42:20 -0400

    Removed old metaedoutput folder

--- MetaEdOutput/SEDM/Database/PostgreSQL/ODS/Structure/1410-EXTENSION-SEDM-CreateIndex-EdOrgIdsRelationship-AuthPerformance.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE INDEX IF NOT EXISTS IX_IDEAEvent_EducationOrganizationId ON sedm.IDEAEvent(EducationOrganizationId) INCLUDE (Id);
-
-CREATE INDEX IF NOT EXISTS IX_IEPServicePrescription_IEPServicingEducationOrganizationId ON sedm.IEPServicePrescription(IEPServicingEducationOrganizationId) INCLUDE (Id);
-
-CREATE INDEX IF NOT EXISTS IX_StudentIEPGoal_IEPServicingEducationOrganizationId ON sedm.StudentIEPGoal(IEPServicingEducationOrganizationId) INCLUDE (Id);
-
-CREATE INDEX IF NOT EXISTS IX_StudentIEPServiceDelivery_IEPServicingEducationOrganizationId ON sedm.StudentIEPServiceDelivery(IEPServicingEducationOrganizationId) INCLUDE (Id);
